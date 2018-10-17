1f648a2f8499a7bbdd1a4f755f0cca8173351ee3
sameerthapliyal <33648097+sameerthapliyal@users.noreply.github.com>
2018-10-17 17:28:52 -0500

    Java encryption and descrypton

---
+++ java/encrydecry/src/main/java/com/coolhead/encrydecry/EncrydecryApplication.java
@@ -0,0 +1,12 @@
+package com.coolhead.encrydecry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncrydecryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncrydecryApplication.class, args);
+	}
+}
---
+++ java/encrydecry/src/test/java/com/coolhead/encrydecry/EncrydecryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coolhead.encrydecry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EncrydecryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
