e4aa28c3bd5a9855109ddb20375b8db0ed5475f1
hothandcoder <24799752+hothandcoder@users.noreply.github.com>
2022-01-06 20:15:31 -0600

    Update Async.java

--- java/concurrent/src/main/java/bbejeck/util/Async.java
+++ java/concurrent/src/main/java/bbejeck/util/Async.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Async {
 
-	//test
+	//test level 2
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CompletableFuture<Void> future = CompletableFuture.runAsync(new Runnable() {
